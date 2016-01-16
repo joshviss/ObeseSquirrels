59860ef455020f00372a31816d283d6e4ba9432a
JoshSauter <sauterj@umich.edu>
2016-01-15 20:55:04 -0500

    Prefab changes as well

--- ForDaManShawn/Assets/__Scripts/PlayerAim.cs
+++ ForDaManShawn/Assets/__Scripts/PlayerAim.cs
@@ -1,8 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//To be attached to the player camera
 public class PlayerAim : MonoBehaviour {
+	public float xSensitivity = 3f;
+	public float ySensitivity = 3f;
 
+	float yAimMin = -80f;	//yAngle clamped between these two values
+	float yAimMax = 80f;    //so we aren't ever looking upside-down
+
+	float x, y;
+
+	Transform playerTransform;
+
+	void Awake() {
+		playerTransform = transform.parent;
+	}
 	// Use this for initialization
 	void Start () {
 	
