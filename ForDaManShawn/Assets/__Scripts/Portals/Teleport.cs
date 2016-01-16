1289ffd77a6fe7c81dc8d6c7f97e0f3fac9ebea5
Alex <alchkim@umich.edu>
2016-01-16 20:01:27 +0900

    portal rotation

--- ForDaManShawn/Assets/__Scripts/Portals/Teleport.cs
+++ ForDaManShawn/Assets/__Scripts/Portals/Teleport.cs
@@ -2,18 +2,53 @@
 using System.Collections;
 
 public class Teleport : MonoBehaviour {
+    public static bool triggered = false;
+    float cdTimer = 1f;
+    float timer;
+    Vector3 posOffset = new Vector3 (0.0f, -0.75f, 0.0f);
+    Vector3 angleOffset = new Vector3 (0.0f, 0.0f, 0.0f);
+    Quaternion objectRot;
+    Vector3 finalRot;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	    
-	}
+    public Transform endPortal;
+    void Start () {
+        timer = cdTimer;
+        finalRot = endPortal.rotation.eulerAngles + angleOffset;
+        objectRot = Quaternion.Euler(finalRot.x, finalRot.y, finalRot.z);
+    }
 
-    void OnCollisionEnter(Collision collision) {
+    void Update () {
+        if (triggered) {
+            print(triggered);
+            timer -= Time.deltaTime;
+            if (timer <= 0) {
+                triggered = false;
+                timer = cdTimer;
+                print(triggered);
+            }
+        }
+    }
+    void OnCollisionEnter (Collision collision) {
+        if (!triggered) {
+            print(collision.collider.name);
+            triggered = true;
+            collision.transform.position = endPortal.position + posOffset;
+            //            Quaternion curRot = collision.transform.rotation;
+            //            collision.transform.Rotate(finalRot.x, finalRot.y, finalRot.z, Space.World);
+            //            collision.transform.rotation = curRot * objectRot;
 
+            if (collision.collider.name == "Player") {
+                Camera portCam = endPortal.GetChild(0).GetComponent<Camera>();
+                var portRot = portCam.transform.rotation.eulerAngles;
+                var tempRot = collision.collider.transform.rotation.eulerAngles;
+ 
+                var temp = collision.collider.transform.GetChild(0);
+                PlayerAim aim = temp.GetComponent<PlayerAim> ();
+ //               var endRot = endPortal.transform.rotation.eulerAngles;
+                aim.portalX += portRot.x;
+                aim.portalY += -portRot.y;
+                aim.portalZ += portRot.z;
+            }
+        }
     }
 }
